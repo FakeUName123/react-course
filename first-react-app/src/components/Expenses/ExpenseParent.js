ac49a95cb457036d3bb327844953a44073a71d44
Grzegorz <grzegorz.siwiec@itmagination.com>
2022-11-10 13:05:34 +0100

    init commit

---
+++ first-react-app/src/components/Expenses/ExpenseParent.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "../Expenses/ExpenseItem";
+import '../Expenses/ExpenseParent.css'
+
+function ExpenseParent(props) {
+  const data = props.data;
+  return (
+    <div className="expenses">
+      {data.map((element) => {
+        return (
+          <ExpenseItem
+            title={element.title}
+            amount={element.amount}
+            date={element.date}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default ExpenseParent;
