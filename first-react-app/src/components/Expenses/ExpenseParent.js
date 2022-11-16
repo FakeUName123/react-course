3560806a5494fac497a2357c0e7c1f064e24a568
Grzegorz <grzegorz.siwiec@itmagination.com>
2022-11-16 16:17:16 +0100

    Expenses Parent JSX restructure

--- first-react-app/src/components/Expenses/ExpenseParent.js
+++ first-react-app/src/components/Expenses/ExpenseParent.js
@@ -5,25 +5,35 @@ import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import Card from "../UI/Card";
 
 function ExpenseParent(props) {
-  const [currState, setState] = useState("2020");
+  const [currState, setState] = useState("2021");
   const onChangeHandler = (selectedOption) => {
     setState(selectedOption);
   };
+
+  const filteredExpenses = props.data.filter((el) => {
+    return el.date.getFullYear() == currState;
+  });
+
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((element) => {
+      return (
+        <ExpenseItem
+          key={element.id}
+          title={element.title}
+          amount={element.amount}
+          date={element.date}
+        />
+      );
+    });
+  }
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={currState} onDrpDwnChange={onChangeHandler} />
-        {props.data.map((element) => {
-          return (
-            <ExpenseItem
-              key={element.id}
-              title={element.title}
-              amount={element.amount}
-              date={element.date}
-            />
-          );
-        })}
+        {expensesContent}
       </Card>
     </div>
   );
