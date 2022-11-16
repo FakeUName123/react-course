ce8f9782a444bc02a2709fb74a53e61cffa95403
Grzegorz <grzegorz.siwiec@itmagination.com>
2022-11-16 13:59:51 +0100

    MINOR property fix

--- first-react-app/src/components/Expenses/ExpenseParent.js
+++ first-react-app/src/components/Expenses/ExpenseParent.js
@@ -5,15 +5,15 @@ import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 function ExpenseParent(props) {
   const data = props.data;
-  const selectedOption = props.
   const [currState, setState] = useState("");
   const onChangeHandler = (selectedOption) => {
     setState(selectedOption);
+    console.log(selectedOption);
   };
 
   return (
     <div className="expenses">
-      <ExpensesFilter selected={currState} onDrpDwnChange={onChangeHandler} />
+      <ExpensesFilter onDrpDwnChange={onChangeHandler} />
       {data.map((element) => {
         return (
           <ExpenseItem
